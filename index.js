8db18f2b9df6de70a2826232a0b31bfd8e5addc2
huseyin <hdonmez@medocity.com>
2016-04-29 18:43:02 -0400

    added middlewares

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import consign from "consign";
 
 const app = express();
 
+consign()
+    .include('boot')
+    .then('route.js')
+    .into(app)
 
 app.get("/", (req,res) => {
     res.status(200).send("Merhaba Node.js");
+
 })
 
 app.listen(3000, () => console.log(`uygulamamiz 3000 nolu port uzerinde calismakta`))
