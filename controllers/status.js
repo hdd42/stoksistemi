11e85dd51d50b0556705f93011da26e1fa948a60
huseyin <hdonmez@medocity.com>
2016-05-01 08:29:08 -0400

    add api doc

---
+++ controllers/status.js
@@ -0,0 +1,14 @@
+/**
+  * @api {get} / API Status
+  * @apiGroup Status
+  * @apiSuccess {String} status API Status' message
+  * @apiSuccessExample {json} Success
+  * HTTP/1.1 200 OK
+  * {"status": "Products API Up and Running"}
+  */
+
+module.exports =(app) =>{
+    app.all('/', (req,res) => {
+        res.status(200).send({status:'Api Up and Running!'})
+        })
+    }
